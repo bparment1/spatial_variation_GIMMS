0d45cdc6152b7a9907429d6d11a7688de668894a
Benoit Parmentier <bparmentier@sshgw02.research.sesync.org>
2018-02-01 14:30:00 -0500

    experimenting with extraction of layers from ncdf

--- GIMMS_processing.R
+++ GIMMS_processing.R
@@ -3,7 +3,7 @@
 ##
 ##
 ## DATE CREATED: 01/24/2018
-## DATE MODIFIED: 01/30/2018
+## DATE MODIFIED: 02/01/2018
 ## AUTHORS: Benoit Parmentier  
 ## Version: 1
 ## PROJECT: spatial variability landscape
@@ -20,22 +20,31 @@
 
 ###### Library used
 
-library(gtools)                              # loading some useful tools 
-
-library(sp)                                  # Spatial pacakge with class definition by Bivand et al.
-library(spdep)                               # Spatial pacakge with methods and spatial stat. by Bivand et al.
-library(raster)                              # raster functions and spatial utilities
-library(rgdal)                               # GDAL wrapper for R, spatial utilities
-library(gdata)                               # various tools with xls reading, cbindX
-library(parallel)                            # Parallelization of processes with multiple cores
-library(maptools)                            # Tools and functions for sp and other spatial objects e.g. spCbind
-library(maps)                                # Tools and data for spatial/geographic objects
-library(plyr)                                # Various tools including rbind.fill
-library(dplyr)                               # data wrangling
-library(rgeos)                               # Geometric, topologic library of functions
-library(gridExtra)                           # Combining lattice plots
-library(colorRamps)                          # Palette/color ramps for symbology
-library(ggplot2)                             # plotting functionality
+library(sp) # spatial/geographic objects and functions
+library(rgdal) #GDAL/OGR binding for R with functionalities
+library(spdep) #spatial analyses operations, functions etc.
+library(gtools) # contains mixsort and other useful functions
+library(maptools) # tools to manipulate spatial data
+library(parallel) # parallel computation, part of base package no
+library(rasterVis) # raster visualization operations
+library(raster) # raster functionalities
+library(forecast) #ARIMA forecasting
+library(xts) #extension for time series object and analyses
+library(zoo) # time series object and analysis
+library(lubridate) # dates functionality
+library(colorRamps) #contains matlab.like color palette
+library(rgeos) #contains topological operations
+library(sphet) #contains spreg, spatial regression modeling
+library(BMS) #contains hex2bin and bin2hex, Bayesian methods
+library(bitops) # function for bitwise operations
+library(foreign) # import datasets from SAS, spss, stata and other sources
+library(gdata) #read xls, dbf etc., not recently updated but useful
+library(classInt) #methods to generate class limits
+library(plyr) #data wrangling: various operations for splitting, combining data
+#library(gstat) #spatial interpolation and kriging methods
+library(readxl) #functionalities to read in excel type data
+library(psych) #pca/eigenvector decomposition functionalities
+library(snow)
 library(sf)
 
 ###### Functions used in this script and sourced from other files
@@ -45,7 +54,7 @@ library(sf)
 #Benoit setup
 script_path <- "/nfs/bparmentier-data/Data/projects/spatial_variation_GIMMS/scripts"
 
-raster_processing_functions <- "GIMMS_processing_functions_01292017b.R" #Functions used to mosaic predicted tiles
+raster_processing_functions <- "GIMMS_processing_functions_02012018.R" #Functions used to mosaic predicted tiles
 source(file.path(script_path,raster_processing_functions)) #source all functions used in this script 
 
 ############################################################################
@@ -67,7 +76,7 @@ scaling_factor <- 0.0001 #MODIFY THE SCALING FACTOR - FOR NORMALIZED DATA SHOULD
 #ARGS 7
 create_out_dir_param=TRUE #create a new ouput dir if TRUE
 #ARGS 8
-out_suffix <-"GIMMS_processing_01292018" #output suffix for the files and ouptut folder #param 12
+out_suffix <-"GIMMS_processing_02012018" #output suffix for the files and ouptut folder #param 12
 num_cores <- 2 # number of cores
 
 ################# START SCRIPT ###############################
@@ -126,7 +135,6 @@ for(i in 1:nf){
 
 GDALinfo_raster <- GDALinfo(raster_file,returnScaleOffset = F) #use GDAL info utility
 
-
 #hdf_file <- file.path(in_dir,hdf_file) #associate file path to input
 #GDALinfo_hdf <- GDALinfo(hdf_file,returnScaleOffset = F) #use GDAL info utility
 
@@ -170,10 +178,36 @@ i <- 2
 raster_file <- lf[i]
 r <- brick(raster_file,"ndvi")
 
-##Assign new names for r?
-names_layers <- names(r)
-names_layers <- gsub("X","m_",names_layers)
+raster_name <- sub(extension(raster_file),"",raster_file)
+list_raster_name <- unlist(strsplit(x=basename(raster_name), split="[_]"))
+year_val <- list_raster_name[[length(list_raster_name)-1]]
+month_range <- list_raster_name[[length(list_raster_name)]]
+month_range <- c(substr(month_range, 1, 2),substr(month_range, 3, 4))
+#month_range <- as.numeric(month_range)
 
+start_date <- paste(year_val,month_range[1],"01",sep=".")
+end_date <- paste(year_val,month_range[2],"30",sep=".")
+
+st <- as.Date(start_date,format="%Y.%m.%d") #start date
+en <- as.Date(end_date,format="%Y.%m.%d") #end date
+ll <- seq.Date(st, en, by="15 day") #sequence of dates
+#dates_queried <- format(ll,"%Y.%m.%d") #formatting queried dates
+dates_val <- format(ll[-13],"%Y_%m_%d") #formatting queried dates
+
+
+#generate dates for 16 days product
+#dates_val <- generate_dates_by_step(date_range[1],date_range[2],16)$dates #NDVI Katrina
+#names_ncdf <- as.character(unlist(strsplit(x=basename(raster_file), split="[_]")))
+#names_ncdf <- names_ncdf[-length(names_ncdf)]
+#names_ncdf <- names_ncdf[-length(names_ncdf)]
+#strsplit(names_ncdf,split="[.]")
+
+##Assign new names for r?
+#names_layers <- names(r)
+#names_layers <- gsub("X","m_",names_layers)
+
+#names(r)
+names(r) <- paste("NDVI",dates_val,sep="_")
 #now drop "." and also later right out the date in the file!!!
 
 raster_name <- paste0(paste(names_ncdf,collapse = "_"),"_",out_suffix,file_format)
@@ -200,7 +234,7 @@ if(is.null(NA_flag_val)){
 
 #r <- readGDAL(modis_subset_layer_Day) #read specific dataset in hdf file and make SpatialGridDataFrame
 
-plot(r,y=1,main="NDVI ~8km",zlim=c(-10000,10000))
+plot(r,y=1,zlim=c(-10000,10000))
 r #print propind the NA flag value
 data_type_str <- dataType(r) #find the dataTyre
 
@@ -236,11 +270,14 @@ strsplit(names_ncdf,split="[.]")
 
 
 #raster_name <- 
-raster_name <- paste0(paste(names_ncdf,collapse = "_"),"_",out_suffix,file_format)
+out_suffix_str <- ""
+#raster_name <- paste0(paste(names_ncdf,collapse = "_"),"_",out_suffix,file_format)
+raster_name <- paste0(paste(names_ncdf,collapse = "_"),file_format)
 
 if(file_format==".tif"){
   format_raster="GTiff"
 }
+
 writeRaster(r,
             filename=file.path(out_dir,raster_name,sep=""),
             bylayer=T,
@@ -308,35 +345,48 @@ r_test <- local_moran_multiple_fun(1,list_param=list_param_moran)
   
 local_moran_I_list <-mclapply(1:nlayers(r_stack), list_param=list_param_moran, 
                       FUN=local_moran_multiple_fun,mc.preschedule=FALSE,
-                      mc.cores = 1) #This is the end bracket from mclapply(...) statement
+                      mc.cores = 2) #This is the end bracket from mclapply(...) statement
 
+r_local_moran_stack <- stack(unlist(local_moran_I_list))
+plot(r_local_moran_stack,y=2)
+animate(r_local_moran_stack)
+
+x_val <- extract(r_local_moran_stack,cbind(-119.6982,34.4208))
+
+plot(x_val[1,],type="l")
+
+x_val <- extract(r_local_moran_stack,cbind(-110.6982,34.4208))
+plot(x_val[1,],type="l")
 
 moran_I_df <-mclapply(1:nlayers(r_stack), list_param=list_param_moran, 
                       FUN=moran_multiple_fun,mc.preschedule=FALSE,
-                      mc.cores = 10) #This is the end bracket from mclapply(...) statement
+                     mc.cores = 2) #This is the end bracket from mclapply(...) statement
 
 moran_df <- do.call(cbind,moran_I_df) #bind Moran's I value 10*nlayers data.frame
 moran_df$lag <-1:nrow(moran_df)
 
+names(moran_df) <- c("moran_I","lag")
+plot(moran_df$moran_I ~moran_df$lag)
+#plot(moran_df$moran_I ~moran_df$lag,ylim=c(0,1))
+
 #prepare to automate the plotting of   all columns
-mydata<-moran_df
-dd <- do.call(make.groups, mydata[,-ncol(mydata)]) 
-dd$lag <- mydata$lag 
+#mydata<-moran_df
+#dd <- do.call(make.groups, mydata[,-ncol(mydata)]) 
+#dd$lag <- mydata$lag 
 
-layout_m<-c(1,3) #one row three columns
-res_pix <-  500
-png(paste("Figure_0b_graphic_abstract_spatial_correlogram_tmax_prediction_models_gam_levelplot_",date_selected,out_prefix,".png", sep=""),
-    height=res_pix*layout_m[1],width=res_pix*layout_m[2])
+#layout_m<-c(1,3) #one row three columns
+#res_pix <-  500
+#png(paste("Figure_0b_graphic_abstract_spatial_correlogram_tmax_prediction_models_gam_levelplot_",date_selected,out_prefix,".png", sep=""),
+#    height=res_pix*layout_m[1],width=res_pix*layout_m[2])
 
-p<-xyplot(data ~ lag | which, dd,type="b",main="Spatial content in interpolated Surfaces using GAM on September 1, 2010",
-          par.settings = list(axis.text = list(font = 2, cex = 1.3),layout=layout_m,
-                              par.main.text=list(font=2,cex=2),strip.background=list(col="white")),par.strip.text=list(font=2,cex=1.5),
-          strip=strip.custom(factor.levels=names_layers),
-          xlab=list(label="Spatial lag neighbor", cex=2,font=2),
-          ylab=list(label="Moran's I", cex=2, font=2))
-print(p)
+#p<-xyplot(data ~ lag | which, dd,type="b",main="Spatial content in interpolated Surfaces using GAM on September 1, 2010",
+#          par.settings = list(axis.text = list(font = 2, cex = 1.3),layout=layout_m,
+#                              par.main.text=list(font=2,cex=2),strip.background=list(col="white")),par.strip.text=list(font=2,cex=1.5),
+#          strip=strip.custom(factor.levels=names_layers),
+#          xlab=list(label="Spatial lag neighbor", cex=2,font=2),
+#          ylab=list(label="Moran's I", cex=2, font=2))
+#print(p)
 
-dev.off()
+#dev.off()
 
 ######################### END OF SCRIPT ##############################
-
